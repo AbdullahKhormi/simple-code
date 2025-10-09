d67c75a3ee0382977ace578e5f67282e9dcced14
AbdullahKhormi <96989807+AbdullahKhormi@users.noreply.github.com>
2025-10-09 14:11:39 +0300

    Update buttons.ts

--- reusable/buttons/buttons.ts
+++ reusable/buttons/buttons.ts
@@ -1,1 +1,20 @@
+import { NgStyle } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+@Component({
+  selector: 'app-buttons',
+  imports: [NgStyle],
+  templateUrl: './buttons.html',
+  styleUrl: './buttons.scss'
+})
+export class Buttons {
+@Input({required:true}) label :string =''
+@Input({required:true}) disabled :boolean =false
+@Input () styles:any={  color: 'yellow',backgroundColor: 'green', fontSize: '18px', padding: '12px 24px'}
+@Input() icon :string=''
+  @Output() buttonClick = new EventEmitter<void>();
+
+  onClick() {
+    this.buttonClick.emit();
+  }
+}
